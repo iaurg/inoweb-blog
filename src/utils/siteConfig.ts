2fbc440268ad4c9674dddecf9b56e82904e05161
Italo A <italoaurelior@gmail.com>
2022-10-19 06:22:26 -0300

    feat: create config file

---
+++ src/utils/siteConfig.ts
@@ -0,0 +1,8 @@
+export const siteConfig = {
+  title: "My Blog",
+  siteName: "My Blog",
+  description: "My blog description",
+  siteUrl: "https://myblog.com",
+  url: "https://myblog.com",
+  ogImage: "/img/opengraph.jpg"
+};
