3441d4e65ce81658552ff23ed18f0ed386c74496
Italo A <italoaurelior@gmail.com>
2022-10-19 06:22:38 -0300

    refactor: home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,75 +1,38 @@
-import Head from "next/head";
+import { NextSeo } from "next-seo";
+import { siteConfig } from "@utils/siteConfig";
 import Layout from "@components/layout";
+import Container from "@components/container";
 
 export default function Index() {
   return (
     <Layout>
-      <Head>
-        <title>Next.js Blog Example with</title>
-      </Head>
-      <h1>Hello</h1>
-    </Layout>
-  );
-}
-
-/*
-
-import { NextSeo } from "next-seo";
-import Layout from "@components/layout";
-import Container from "@components/container";
-import { useRouter } from "next/router";
-import defaultOG from "../public/img/opengraph.jpg";
-import { postquery, configQuery } from "@lib/groq";
-import GetImage from "@utils/getImage";
-import PostList from "@components/postlist";
-
-export default function Post(props) {
-  const { postdata, siteconfig, preview } = props;
-
-  const router = useRouter();
-
-  const { data: posts } = usePreviewSubscription(postquery, {
-    initialData: postdata,
-    enabled: preview || router.query.preview !== undefined
-  });
-
-  const { data: siteConfig } = usePreviewSubscription(configQuery, {
-    initialData: siteconfig,
-    enabled: preview || router.query.preview !== undefined
-  });
-
-  const ogimage = siteConfig?.openGraphImage
-    ? GetImage(siteConfig?.openGraphImage).src
-    : defaultOG.src;
-
-  return (
-    <>
-      {posts && siteConfig && (
-        <Layout {...siteConfig}>
-          <NextSeo
-            title={`${siteConfig?.title}`}
-            description={siteConfig?.description || ""}
-            canonical={siteConfig?.url}
-            openGraph={{
-              url: siteConfig?.url,
-              title: `${siteConfig?.title}`,
-              description: siteConfig?.description || "",
-              images: [
-                {
-                  url: ogimage,
-                  width: 800,
-                  height: 600,
-                  alt: ""
-                }
-              ],
-              site_name: "inoweb"
-            }}
-            twitter={{
-              cardType: "summary_large_image"
-            }}
-          />
-          <Container>
-            <div className="grid gap-10 lg:gap-10 md:grid-cols-2 ">
+      <NextSeo
+        title={`${siteConfig.title}`}
+        description={siteConfig.description}
+        canonical={siteConfig.url}
+        openGraph={{
+          url: siteConfig.url,
+          title: `${siteConfig?.title}`,
+          description: siteConfig?.description,
+          images: [
+            {
+              url: siteConfig.ogImage,
+              width: 800,
+              height: 600,
+              alt: ""
+            }
+          ],
+          site_name: siteConfig.siteName
+        }}
+        twitter={{
+          cardType: "summary_large_image"
+        }}
+      />
+      <Container>
+        <div className="grid gap-10 lg:gap-10 md:grid-cols-2 ">
+          oi
+          {/*
+          <div className="grid gap-10 lg:gap-10 md:grid-cols-2 ">
               {posts.slice(0, 2).map(post => (
                 <PostList
                   key={post._id}
@@ -88,27 +51,9 @@ export default function Post(props) {
                 />
               ))}
             </div>
-          </Container>
-        </Layout>
-      )}
-    </>
+           */}
+        </div>
+      </Container>
+    </Layout>
   );
 }
-
-export async function getStaticProps({ params, preview = false }) {
-  const post = await getClient(preview).fetch(postquery);
-  const config = await getClient(preview).fetch(configQuery);
-
-  // const categories = (await client.fetch(catquery)) || null;
-
-  return {
-    props: {
-      postdata: post,
-      // categories: categories,
-      siteconfig: { ...config },
-      preview
-    },
-    revalidate: 10
-  };
-}
-*/
