4ba4c8679467522cc7bdc919aa1489a559080256
Italo A <italoaurelior@gmail.com>
2022-10-20 21:04:17 -0300

    feat: remove custom loader

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   images: {
-    domains: ["inoweb.com.br"],
-    loader: "custom"
+    domains: ["inoweb.com.br"]
+    // loader: "custom"
   },
   swcMinify: true,
   experimental: {
