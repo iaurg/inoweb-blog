7e1efa05892e93d330f45ac4eb111f14d2d234fc
Italo A <italoaurelior@gmail.com>
2022-10-20 21:01:57 -0300

    feat: translate footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,14 +1,12 @@
 import Container from "@components/container";
 import ThemeSwitch from "@components/themeSwitch";
-import Image from "next/image";
-import { myLoader } from "@utils/all";
 
 export default function Footer(props) {
   return (
     <Container className="mt-10 border-t border-gray-100 dark:border-gray-800">
       <div className="text-sm text-center">
-        Copyright © {new Date().getFullYear()} {props?.copyright}. All
-        rights reserved.
+        Copyright © {new Date().getFullYear()} {props?.copyright}.
+        Todos os direitos reservados.
       </div>
       <div className="flex items-center justify-between mt-2">
         <ThemeSwitch />
