ab1196874327334b2b15f5e998f0e333be46eea5
Italo A <italoaurelior@gmail.com>
2022-10-28 09:48:50 -0300

    fix: theme switcher

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -5,11 +5,11 @@ export default function Footer(props) {
   return (
     <Container className="mt-10 border-t border-gray-100 dark:border-gray-800">
       <div className="text-sm text-center">
+        <div className="mb-4">
+          <ThemeSwitch />
+        </div>
         Copyright © {new Date().getFullYear()} {props?.copyright}.
         Todos os direitos reservados.
-      </div>
-      <div className="flex items-center justify-between mt-2">
-        <ThemeSwitch />
       </div>
     </Container>
   );
--- src/components/themeSwitch.tsx
+++ src/components/themeSwitch.tsx
@@ -1,24 +1,14 @@
-// import { useState, useEffect } from "react";
 import { useTheme } from "next-themes";
 import { SunIcon } from "@heroicons/react/outline";
 
 const ThemeSwitch = () => {
-  //   const [mounted, setMounted] = useState(false);
-  //   const { resolvedTheme, setTheme } = useTheme();
   const { theme, setTheme } = useTheme();
-  // useEffect only runs on the client, so now we can safely show the UI
-  //   useEffect(() => {
-  //     setMounted(true);
-  //   }, []);
-
-  //   if (!mounted) {
-  //     return null;
-  //   }
 
   return (
     <div className="inline-flex items-center">
       <SunIcon className="w-4 h-4 mr-2" />
       <select
+        className="text-sm font-medium text-gray-600 dark:text-gray-400 p-2 rounded-md bg-gray-100 dark:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50"
         name="themeSwitch"
         value={theme}
         onChange={e => setTheme(e.target.value)}>
